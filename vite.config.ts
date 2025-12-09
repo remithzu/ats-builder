949e419d70dd877f7aa72cceb02839fe7bc96b49
Remithzu <remithzu@github.com>
2025-12-09 09:37:43 +0700

    fix: deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
+// Detect if building for GitHub Pages
+const isGhPages = process.env.GITHUB_PAGES === 'true';
+
 export default defineConfig({
+  base: isGhPages ? '/ats-builder/' : '/',
   plugins: [react()],
   server: {
     port: 3000,
